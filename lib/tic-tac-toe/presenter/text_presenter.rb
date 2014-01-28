495b09276645bc85636d77a8af94a3dac4661ccf
Chris Woodford <chris.woodford@gmail.com>
2014-01-28 16:11:17 -0500

    update the board printing to include row and column headers

--- lib/tic-tac-toe/presenter/text_presenter.rb
+++ lib/tic-tac-toe/presenter/text_presenter.rb
@@ -15,21 +15,21 @@ module TicTacToe
       def print_board(board)
 
         col_headers = ["1", "2", "3"]
+
         row_headers = ["A", "B", "C"]
-        row_counter = 0
+        row_counter = -1
 
         @output.puts "   #{col_headers.join("   ")} "
 
-        board.to_a.map { |row| " #{row.join(" | ")} " }
-          .insert(1, "-----------")
-          .insert(3, "-----------")
-          .each_with_index do |row, index|
-            row_header = " "
-            if index.even?
-              row_header = "#{row_headers[row_counter]}"
-              row_counter += 1
-            end
-            @output.puts "#{row_header} #{row}"
+        board.to_a
+          .map do |row|
+            row_counter += 1
+            "#{row_headers[row_counter]}  #{row.join(" | ")} "
+          end
+          .insert(1, "  -----------")
+          .insert(3, "  -----------")
+          .each do |row|
+            @output.puts row
           end
 
       end
